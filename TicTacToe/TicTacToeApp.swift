1bffc1d71cccab2162470f28b6956a1008bc3b43
David <dverbenyi@gmail.com>
2022-05-26 13:33:43 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by David Verbenyi on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
